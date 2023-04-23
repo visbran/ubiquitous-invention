94e5f0bfee7614dfd8d8c6cde5e36406fcf54229
Brandon Visca <brandon@Pixou-Air-001.lan>
2023-04-23 22:52:45 +0200

    update and fix auto

--- components/NavBar/Header.js
+++ components/NavBar/Header.js
@@ -179,8 +179,8 @@ const Header = ({ navBarTitle, fullWidth }) => {
               <Image
                   src='/favicon.png'
                   alt='Logo'
-                  width={100}
-                  height={100}
+                  width={90}
+                  height={90}
               />
             </motion.div>
           </Link>
